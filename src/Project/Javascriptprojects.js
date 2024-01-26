62b1ba7555267e9dede0b340a66d18444bc220f8
nagavasu45 <nagavasu450@gmail.com>
2024-01-26 11:15:05 +0530

    commit120

--- src/Project/Javascriptprojects.js
+++ src/Project/Javascriptprojects.js
@@ -35,37 +35,7 @@ const Javascriptprojects = () => {
       </div>
 
     </div>
-    </div>
-    {/* <div className='html-container3' >
-
-    
-    <div >Todo App Js <br/>
-    <hr/></div>
-    
-    <div className='html-container2'>
-      
-      <div className='html-container2-sub1'>
-        Host Link:
-        <div className='html-container2-sub1-item1'>https://nagavasu45.github.io/dynamictodo/</div>
-        Repo Link:
-        <div className='html-container2-sub1-item2'>
-        https://github.com/Nagavasu45/dynamictodo</div>
-      </div>
-      <div className='html-container2-sub2'>
-        <div className='html-container2-sub2-text1'>
-          Project Description:
-        </div>
-        <div className='html-container2-sub2-text2'>
-          This is completely HTML based project. <br/>
-          In this project i have used basic styles and html5 tags only <br/>
-          I have faced soo many chanllanges like put the images in table cells and
-          cell padding and cell spacing and row, coll sapn for merging cells in table
-
-        </div>
-      </div>
-
-    </div>
-    </div> */}
+    </div>   
     </>)
 }
 
